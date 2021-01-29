4aeb9b67695cfacfd5e6951a9c220304628da17f
Hesston Robertson <hestonlolzerton@gmail.com>
2021-01-29 17:17:54 -0600

    added to gitignore

--- SyntaxTerror.py
+++ SyntaxTerror.py
@@ -1,4 +1,29 @@
 import json
 import asyncio
 import discord
+import logging
+from discord.ext import commands
+import math
 
+#Retrieve token from config.json
+with open('config.json') as jsonData:
+    Token = json.load(jsonData)
+    Token = Token.get('Token')
+
+logging.basicConfig(level=logging.INFO)
+
+bot = commands.Bot(command_prefix ='.')
+
+
+@bot.event
+async def on_ready():
+    print('Bot Online')
+
+
+@bot.command()
+async def ping(ctx):
+    '''Displays latency'''
+    await ctx.send("{0}ms".format(math.floor(bot.latency*1000)))
+
+
+bot.run(Token)
